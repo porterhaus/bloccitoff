16c38799387c2ebbd816b10b00ee625a99dc6f19
porterhaus <bporter@porterhaus.com>
2015-02-17 11:27:06 -0600

    Generated Lists controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :lists
+
   devise_for :users
   devise_for :views
+  devise_for :items
   get '/about', to: 'pages#about', as: 'about'
 
   root to: 'pages#index'
